9c086e3bd7b0940fbad34f2fe018b237ccf4ffb5
cuipengfei <cuipengfei2008@gmail.com>
2014-02-23 23:40:13 +0800

    create the server and the client only once in all tests. those 13 tests used to take 4 seconds, not only 2.

--- src/test/java/com/jersey/whatnot/JerseyAwareTest.java
+++ src/test/java/com/jersey/whatnot/JerseyAwareTest.java
@@ -1,10 +1,7 @@
 package com.jersey.whatnot;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.junit.After;
-import org.junit.Before;
 
-import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 
 import static com.jersey.whatnot.GrizzlyServer.BASE_URI;
@@ -12,19 +9,11 @@ import static com.jersey.whatnot.GrizzlyServer.startServer;
 import static javax.ws.rs.client.ClientBuilder.newClient;
 
 public class JerseyAwareTest {
-    protected HttpServer server;
-    protected WebTarget target;
+    protected static HttpServer server;
+    protected static WebTarget target;
 
-    @Before
-    public void setUp() throws Exception {
+    static {
         server = startServer();
-
-        Client client = newClient();
-        target = client.target(BASE_URI);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.shutdownNow();
+        target = newClient().target(BASE_URI);
     }
 }
