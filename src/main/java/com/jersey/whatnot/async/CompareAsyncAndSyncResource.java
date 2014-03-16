9b2178a653dc09c1d82c8d488288184c2042ddd8
cuipengfei <cuipengfei2008@gmail.com>
2014-03-16 18:52:32 +0800

    ah, seems like jersey is using a different thread to process every request, but why does the document seems to indicate otherwise ?

--- src/main/java/com/jersey/whatnot/async/CompareAsyncAndSyncResource.java
+++ src/main/java/com/jersey/whatnot/async/CompareAsyncAndSyncResource.java
@@ -12,7 +12,7 @@ public class CompareAsyncAndSyncResource {
     @GET
     @Path("sync-call")
     public String syncCall() throws InterruptedException {
-        System.out.println("got request");
+        System.out.println("got request at: " + Thread.currentThread().getId());
         expensiveComputation();
         System.out.println("computation done");
         return "sync call finished";
@@ -29,7 +29,7 @@ public class CompareAsyncAndSyncResource {
                 System.out.println("resumed");
             }
         }).start();
-        System.out.println("got request");
+        System.out.println("got request at: " + Thread.currentThread().getId());
     }
 
     private void expensiveComputation() {
