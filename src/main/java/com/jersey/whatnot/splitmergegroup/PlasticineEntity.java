a992b02c096ab79cd3f5e214a3bdc3b6a6385400
cuipengfei <cuipengfei2008@gmail.com>
2014-03-29 15:49:05 +0800

    可以把父子关系用一个实体映射进来了

--- src/main/java/com/jersey/whatnot/splitmergegroup/PlasticineEntity.java
+++ src/main/java/com/jersey/whatnot/splitmergegroup/PlasticineEntity.java
@@ -1,17 +1,32 @@
 package com.jersey.whatnot.splitmergegroup;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "plasticine")
 public class PlasticineEntity {
+    @Id
+    @Column(name = "id")
     private Integer id;
 
-    @Id
-    @Column(name = "id", nullable = false, insertable = true, updatable = true, length = 10, precision = 0)
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(name = "plasticine_relation",
+            joinColumns = {@JoinColumn(name = "oldPlasticineId")},
+            inverseJoinColumns = {@JoinColumn(name = "newPlasticineId")})
+    private Set<PlasticineEntity> children = new HashSet<PlasticineEntity>();
+
+    @ManyToMany(mappedBy = "children")
+    private Set<PlasticineEntity> parents = new HashSet<PlasticineEntity>();
+
     public Integer getId() {
         return id;
     }
